e0d1c324c8bdb0916f9a0583a68b47c8b844c937
Tristan <tristanengst@gmail.com>
2023-03-21 01:36:47 -0700

    JOBID no longer in model names. I think this resolves an issue with WandB resuming slurm runs

--- IMLE_DAE.py
+++ IMLE_DAE.py
@@ -28,9 +28,8 @@ torch.multiprocessing.set_sharing_strategy("file_system")
 def imle_model_folder(args, make_folder=False):
     data_str = Data.dataset_pretty_name(args.data_tr)
     suffix = "" if args.suffix is None else f"-{args.suffix}"
-    job_id = "" if args.job_id is None else f"-{args.job_id}"
     lrs = "_".join([f"{lr:.2e}" for idx,lr in enumerate(args.lrs) if idx % 2 == 1])
-    folder = f"{args.save_folder}/models_{args.script}/{args.script}-{data_str}-bs{args.bs}-epochs{args.epochs}-feat_dim{args.feat_dim}-ipe{args.ipe}-leakyrelu{args.leaky_relu}-lr{lrs}-ns{args.ns}-nshot{args.n_shot}-nway{args.n_way}-seed{args.seed}-{args.uid}{job_id}{suffix}"
+    folder = f"{args.save_folder}/models_{args.script}/{args.script}-{data_str}-bs{args.bs}-epochs{args.epochs}-feat_dim{args.feat_dim}-ipe{args.ipe}-leakyrelu{args.leaky_relu}-lr{lrs}-ns{args.ns}-nshot{args.n_shot}-nway{args.n_way}-seed{args.seed}-{args.uid}{suffix}"
 
     if make_folder:
         Utils.conditional_make_folder(folder)
